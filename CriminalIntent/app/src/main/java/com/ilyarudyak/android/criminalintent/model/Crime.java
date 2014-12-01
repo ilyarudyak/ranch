abd100269cddf103490f8d98ebd9e8c1646a0936
Ilya Rudyak <ilyarudyak@yahoo.com>
2014-12-01 22:13:35 +0300

    add model class Crime

---
+++ CriminalIntent/app/src/main/java/com/ilyarudyak/android/criminalintent/model/Crime.java
@@ -0,0 +1,29 @@
+package com.ilyarudyak.android.criminalintent.model;
+
+import java.util.UUID;
+
+/**
+ * Created by ilyarudyak on 01/12/14.
+ */
+public class Crime {
+
+    private UUID mId;
+
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+}
